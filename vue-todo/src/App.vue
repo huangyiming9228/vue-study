6b952bfe92d4e87b54a151a999987d6b57652406
黄从富 <764520351@qq.com>
2018-09-13 12:22:18 +0800

    添加todo

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="app">
+        <create-todo @create-todo="addTodo"></create-todo>
+		<todo-list
+            v-bind:todos="todos"
+        ></todo-list>
+	</div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from "./components/CreateTodo";
+
+export default {
+	name: 'App',
+	components: {
+        TodoList,
+        CreateTodo
+    },
+    data() {
+        return {
+            todos: [
+                {
+                    title: 'Todo A',
+                    project: 'Project A',
+                    done: false
+                },
+                {
+                    title: 'Todo B',
+                    project: 'Project B',
+                    done: false
+                },
+                {
+                    title: 'Todo C',
+                    project: 'Project C',
+                    done: false
+                }
+            ]
+        };
+    },
+    methods: {
+        addTodo(todo) {
+            this.todos.push(todo);
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>已完成的项目数：{{ todos.filter(item => {return item.done === true}).length }}</p>
+        <p>未完成的项目数：{{ todos.filter(item => {return item.done === false}).length }}</p>
+        <todo 
+            v-for="(todo, index) in todos" 
+            :key="index" 
+            :todo.sync="todo"
+            @delete-todo="deleteTodo"
+            @complete-todo="completeTodo"
+        ></todo>
+    </div>
+</template>
+
+<script>
+import Todo from './Todo';
+export default {
+    props: ['todos'],
+    components: {
+        Todo
+    },
+    methods: {
+        deleteTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        },
+        completeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos[todoIndex].done = true;
+            console.log(this.todos);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

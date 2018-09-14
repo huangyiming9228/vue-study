232843eb73a56e2574c625360c6569feec962090
黄从富 <764520351@qq.com>
2018-09-14 12:36:41 +0800

    添加vue modal

---
+++ vue-modal/README.md
@@ -0,0 +1,20 @@
+# vue-modal
+
+> a universal vue modal
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+总结：熟悉了vue中的事件修饰符、slot插槽、css过渡的使用。
---
+++ vue-modal/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <button type="button" class="btn" @click="showModal">打开Modal</button>
+        <modal v-show="isModalVisible" @close-modal="closeModal">
+            <template slot="body">
+                <label>姓名：<input type="text"></label>
+            </template>
+        </modal>
+    </div>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+export default {
+    name: 'App',
+    components: {
+        Modal
+    },
+    data() {
+        return {
+            isModalVisible: false
+        };
+    },
+    methods: {
+        showModal() {
+            this.isModalVisible = true;
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>

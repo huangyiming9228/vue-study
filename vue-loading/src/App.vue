0e1d95bdf8b465a6801465ddb1fa58991ad9850c
黄从富 <764520351@qq.com>
2018-09-15 15:22:28 +0800

    添加loading参数控制面板

--- vue-loading/src/App.vue
+++ vue-loading/src/App.vue
@@ -6,6 +6,24 @@
 			:dot-color-val="dotColor"
 			:dot-nums="dotNums"
 		></loading>
+		<div class="control">
+			<div class="control-form">
+				<label for="dotNums">圆点个数:</label>
+				<input type="number" step="1" min="8" max="20" id="dotNums" v-model.number="dotNums"   />
+			</div>
+			<div class="control-form">
+				<label for="loadingRadius">容器半径:</label>
+				<input type="range" min="50" max="200" step="1" id="loadingRadius" v-model.number="loadingRadius"  />
+			</div>
+			<div class="control-form">
+				<label for="dotRadius">圆点半径:</label>
+				<input type="range" min="8" max="30" step="1" id="dotRadius" v-model.number="dotRadius"  />
+			</div>
+			<div class="control-form">
+				<label for="dotColor">圆点颜色:</label>
+				<input type="color" v-model="dotColor" id="dotColor"  />
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -46,5 +64,8 @@ body {
 	> div {
 		margin: 0 2rem;
 	}
+}
+.control-form {
+  margin: 10px 0;
 }
 </style>
